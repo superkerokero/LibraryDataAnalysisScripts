c659daac3e219045e472b8742c6424eb108d624f
Lingjian Wang <superkerokero@outlook.com>
2016-10-05 23:00:30 +0900

    Added output for GraphML format for analysis using Cytoscape.

--- readcsv.py
+++ readcsv.py
@@ -7,14 +7,13 @@ import matplotlib.pyplot as plt
 import networkx as nx
 
 
-
 if __name__ == "__main__":
     csvobj = csv2Graph.csv2Graph("data/libsample.csv")
     nodes = csvobj.createNodeList(7)
     relation = csvobj.createNodeList(11)
-    edges = csvobj.createEdgeList(nodes, {11:relation})
+    edges = csvobj.createEdgeList(nodes, {11: relation})
     graph = csvobj.createGraph(nodes, edges)
     nx.draw(graph)
-    print(nodes, edges)
+    nx.write_graphml(graph, "test.graphml")
     print("Number of edges in graph: ", graph.number_of_edges())
     plt.show()
